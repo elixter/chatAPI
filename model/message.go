6d83b303f1c27f88d4b73410a35fd33831cca87a
이태원 <ltw971@naver.com>
2022-06-03 21:31:26 +0900

    fix: Message.content type as json.RawMessage

--- model/message.go
+++ model/message.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type MessageType string
 
@@ -10,10 +13,10 @@ const (
 )
 
 type Message struct {
-	Id          int64       `json:"id"`
-	MessageType MessageType `json:"message_type"`
-	AuthorId    int64       `json:"author_id"`
-	RoomId      int64       `json:"room_id"`
-	Content     []byte      `json:"content"`
-	CreateAt    time.Time   `json:"create_at"`
+	Id          int64           `json:"id"`
+	MessageType MessageType     `json:"message_type"`
+	AuthorId    int64           `json:"author_id"`
+	RoomId      int64           `json:"room_id"`
+	Content     json.RawMessage `json:"content"`
+	CreateAt    time.Time       `json:"create_at"`
 }
