3fca95f90e71cfa30679ca5efa4ba91f215732bc
elixter <ltw971@naver.com>
2022-06-28 17:19:25 +0900

    feat: room repository

--- server/model/room.go
+++ server/model/room.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Room struct {
-	Id       int64     `json:"id"`
-	Name     string    `json:"name"`
-	Private  bool      `json:"private"`
-	CreateAt time.Time `json:"create_at"`
+	Id       int64     `json:"id" db:"id"`
+	Name     string    `json:"name" db:"name"`
+	Private  bool      `json:"private" db:"private"`
+	CreateAt time.Time `json:"create_at" db:"create_at"`
 }
